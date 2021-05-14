1b1c8833a13d6f0ec89e7b7883ea121c7166fb17
Connor DiPietro <connor.dipietro@gmail.com>
2021-05-14 17:51:23 -0400

    adds: bootstrap and app container

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Container from "./components/container";
 
 
 ReactDOM.render(
   <React.StrictMode>
-    <h1>Test</h1>
+    <Container />
   </React.StrictMode>,
   document.getElementById('root')
 );
