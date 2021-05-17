c87f2592dd7cb1679793c4947229cad6d42bce19
Connor DiPietro <connor.dipietro@gmail.com>
2021-05-17 16:59:43 -0400

    formats accordion borders

--- src/components/AboutAccordian.js
+++ src/components/AboutAccordian.js
@@ -3,12 +3,12 @@ import { Accordion, Card } from 'react-bootstrap';
 const AboutAccordion = () => {
   return (
     <Accordion defaultActiveKey="0">
-        <Card >
-      <Accordion.Toggle as={Card.Header} eventKey="1">
+        <Card className="acord-btm">
+      <Accordion.Toggle as={Card.Header} eventKey="1" className="acord-btm">
         Skills
       </Accordion.Toggle>
       <Accordion.Collapse eventKey="1">
-        <Card.Body  className="pb-1">
+        <Card.Body  className="pb-1 acord-btm">
         <p>Front End: React/Redux, Backbone, Bootstrap, Handlebars, ES6, HTML5, CSS3</p>
         <hr></hr>
         <p>Back End: Node, Express, Mongo/Mongoose, Auth, Swagger</p>
@@ -19,33 +19,33 @@ const AboutAccordion = () => {
         </Card.Body>
       </Accordion.Collapse>
     </Card>
-    <Card>
-      <Accordion.Toggle as={Card.Header} eventKey="2">
+    <Card className="square acord-top">
+      <Accordion.Toggle as={Card.Header} eventKey="2" className="square acord-top">
         Resume
       </Accordion.Toggle>
       <Accordion.Collapse eventKey="2">
-        <Card.Body className="pb-1">
+        <Card.Body className="pb-1 acord-btm">
         <p>Download</p>
         </Card.Body>
       </Accordion.Collapse>
     </Card>
-    <Card>
-      <Accordion.Toggle as={Card.Header} eventKey="3">
+    <Card className="square">
+      <Accordion.Toggle as={Card.Header} eventKey="3" className="square">
         About this site
       </Accordion.Toggle>
       <Accordion.Collapse eventKey="3">
-        <Card.Body className="pb-1">
+        <Card.Body className="pb-1 square">
           <p>Built with React, stylized with Bootstrap, and continuiously deployed via AWS Amplify, Certificate Manager, &amp; Route 53.</p> <hr></hr><p>Check it out on <u>
             <a href="https://github.com/connordipietro/c-dip" target="_blank" rel="noreferrer noopener"> GitHub ↗</a></u></p>
         </Card.Body>
       </Accordion.Collapse>
     </Card>
-    <Card>
-      <Accordion.Toggle as={Card.Header} eventKey="4">
+    <Card className="acord-top">
+      <Accordion.Toggle as={Card.Header} eventKey="4" className="acord-top">
         What's next
       </Accordion.Toggle>
-      <Accordion.Collapse eventKey="4">
-        <Card.Body className="pb-1">
+      <Accordion.Collapse eventKey="4" className="acord-top">
+        <Card.Body className="pb-1 acord-top">
         <p>AWS &amp; Swift!</p>
         </Card.Body>
       </Accordion.Collapse>
