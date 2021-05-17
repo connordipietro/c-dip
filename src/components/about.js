9be31da66f7773414ffa8d4bda47bb68a4406537
Connor DiPietro <connor.dipietro@gmail.com>
2021-05-16 21:46:44 -0400

    fixes m and g on aboutaccordian

--- src/components/about.js
+++ src/components/about.js
@@ -8,12 +8,12 @@ const About = () => {
         <p className="lead">Hello, world!</p>
         <p>Based in Washington D.C. and open to relocation, I am a Fire Fighter turned Software Engineering Student with a passion for technology and solving complex problems.</p>
         <p>Working with unresponsive and outdated digital tools in the firehouse inspired me to find a better way. My goal is to work with a team and to create meaningful products that better people's lives.</p>
-        <div className="row row-cols-1 row-cols-md-2 g-4 pb-5 mx-3 pt-4">
+        <div className="row row-cols-1 row-cols-md-2 g-4 pt-2">
           <div className="col">
             <AboutAccordion></AboutAccordion>
           </div>
           <div className="col text-center">
-            <img width={250} height={250} className="mr-3" src="https://avatars.githubusercontent.com/u/74513418?v=4" alt="Generic placeholder"/>
+            <img width={250} height={250} src="https://avatars.githubusercontent.com/u/74513418?v=4" alt="Generic placeholder"/>
           </div>
         </div>
       </Jumbotron>
